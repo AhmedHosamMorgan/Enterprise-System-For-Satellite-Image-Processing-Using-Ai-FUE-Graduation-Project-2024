bc652b554a8f9e56dd094760703415ee7f3c4830
Ahmed HossamEldeen Elsaeed <73781694+AhmedHosamMorgan@users.noreply.github.com>
2024-04-18 15:33:37 +0200

    Update views.py

--- ajax_app/views/super_resoulation/views.py
+++ ajax_app/views/super_resoulation/views.py
@@ -5,59 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
 
-def EsrganImage (img_name) : 
-    
-    output = None
-    if img_name == 'Cairo.png' : 
-        output = "static/Dataset/2X/Cairo2X.png" 
-    
-    if img_name == 'Delta.png' : 
-        output = "static/Dataset/2X/Delta4X.png" 
-    
-    if img_name == 'Fayoum.png' : 
-        output = "static/Dataset/2X/Fayoum2X.png"
-    
-    if img_name == 'NasserLake.png' : 
-        output = "static/Dataset/2X/NasserLake2X.png" 
-
-    if img_name == 'PortSaid.png' : 
-        output = "static/Dataset/2X/PortSaid2X.png"
-
-    if img_name == 'Qena.png' : 
-        output = "static/Dataset/2X/Qena2X.png"
-
-    if img_name == 'Sharm.png' : 
-        output = "static/Dataset/2X/Sharm2X.png"
-    
-    return output
-
-
-def EdsrImage (img_name) : 
-    
-    output = None
-    if img_name == 'Cairo.png' : 
-        output = "static/Dataset/4X/Cairo4X.png" 
-    
-    if img_name == 'Fayoum.png' : 
-        output = "static/Dataset/4X/Fayoum4X.png"
-
-    if img_name == 'Delta.png' : 
-        output = "static/Dataset/4X/Delta4X.png"
-    
-    if img_name == 'NasserLake.png' : 
-        output = "static/Dataset/4X/NasserLake4X.png" 
-    
-    if img_name == 'PortSaid.png' : 
-        output = "static/Dataset/4X/PortSaid4X.png"
-
-    if img_name == 'Qena.png' : 
-        output = "static/Dataset/4X/Qena4X.png"
-
-    if img_name == 'Sharm.png' : 
-        output = "static/Dataset/4X/Sharm4X.png"
-    
-    return output
-
 
 @csrf_exempt
 def ESRGAN (request) : 
